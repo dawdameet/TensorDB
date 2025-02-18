6e91d87f56143e98e1657d2f5c4e0bf0755bb61e
Meet Dawda <94282163+dawdameet@users.noreply.github.com>
2025-02-18 22:35:32 +0530

    Implemented basic MatMul in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use std::vec;
+
+use jni::JNIEnv;
+use jni::objects::JClass;
+use jni::sys::jint;
+use ndarray::{Array2,Axis};
 #[no_mangle]
-pub extern "C" fn processTensor(){
-    println!("Processing tensors.S")
+pub extern "system" fn processTensor(){
+    println!("Processing Tensors...");
+    let mat: Array2<f32>=Array2::from_shape_vec((2,2), vec![1.0,2.0,3.0])
+            .expect("Tensor process failed.");
+    let res=mat.dot(&mat);
+    println!("Multiplied Matrix: \n{}",res);
+    0
 }
